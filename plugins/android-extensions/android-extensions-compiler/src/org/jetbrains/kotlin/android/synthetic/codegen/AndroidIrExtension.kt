fe60393e48ebbd75d31d1264693a47fd56139818
Alexander Udalov <alexander.udalov@jetbrains.com>
2022-02-03 00:46:03 +0100

    Android extensions: do not produce unbound symbol for mutableMapOf
     #KT-50784 Fixed
    
    (cherry picked from commit 210cc6704109d70290f2944fb24bcdf58d4f2fae)

--- plugins/android-extensions/android-extensions-compiler/src/org/jetbrains/kotlin/android/synthetic/codegen/AndroidIrExtension.kt
+++ plugins/android-extensions/android-extensions-compiler/src/org/jetbrains/kotlin/android/synthetic/codegen/AndroidIrExtension.kt
@@ -25,10 +25,7 @@ import org.jetbrains.kotlin.ir.builders.declarations.*
 import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.declarations.impl.IrExternalPackageFragmentImpl
 import org.jetbrains.kotlin.ir.declarations.impl.IrFactoryImpl
-import org.jetbrains.kotlin.ir.expressions.IrCall
-import org.jetbrains.kotlin.ir.expressions.IrExpression
-import org.jetbrains.kotlin.ir.expressions.IrStatementOrigin
-import org.jetbrains.kotlin.ir.expressions.IrTypeOperator
+import org.jetbrains.kotlin.ir.expressions.*
 import org.jetbrains.kotlin.ir.expressions.impl.*
 import org.jetbrains.kotlin.ir.symbols.IrClassifierSymbol
 import org.jetbrains.kotlin.ir.symbols.IrSimpleFunctionSymbol
@@ -119,8 +116,8 @@ private class AndroidIrTransformer(val extension: AndroidIrExtension, val plugin
         }
 
     // NOTE: sparse array version intentionally not implemented; this plugin is deprecated
-    private val mapFactory = pluginContext.irBuiltIns.findFunctions(Name.identifier("mutableMapOf"), "kotlin", "collections")
-        .single { it.descriptor.valueParameters.isEmpty() } // unbound - can't use `owner`
+    private val mapFactory = pluginContext.referenceFunctions(FqName("kotlin.collections.mutableMapOf"))
+        .single { it.owner.valueParameters.isEmpty() }
     private val mapGet = pluginContext.irBuiltIns.mapClass.owner.functions
         .single { it.name.asString() == "get" && it.valueParameters.size == 1 }
     private val mapSet = pluginContext.irBuiltIns.mutableMapClass.owner.functions
