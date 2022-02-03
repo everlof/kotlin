cd6d391b3b8ff2d08417c597531b292e1c96433b
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2022-02-03 14:04:28 +0300

    Native: terminate if cinterop ObjC reference conversion throws exception
    To avoid forwarding Obj-C exceptions to Kotlin or vice versa.
    Otherwise this might lead to a crash or other undesirable behaviour.
    
    ^KT-50648 Fixed

--- kotlin-native/runtime/src/main/cpp/ObjCExport.mm
+++ kotlin-native/runtime/src/main/cpp/ObjCExport.mm
@@ -566,11 +566,11 @@ extern "C" id Kotlin_ObjCExport_refToLocalObjC(ObjHeader* obj) {
   return Kotlin_ObjCExport_refToObjCImpl<false>(obj);
 }
 
-extern "C" ALWAYS_INLINE id Kotlin_Interop_refToObjC(ObjHeader* obj) {
+extern "C" ALWAYS_INLINE id Kotlin_Interop_refToObjC(ObjHeader* obj) noexcept {
   return Kotlin_ObjCExport_refToObjCImpl<false>(obj);
 }
 
-extern "C" ALWAYS_INLINE OBJ_GETTER(Kotlin_Interop_refFromObjC, id obj) {
+extern "C" ALWAYS_INLINE OBJ_GETTER(Kotlin_Interop_refFromObjC, id obj) noexcept {
   // TODO: consider removing this function.
   RETURN_RESULT_OF(Kotlin_ObjCExport_refFromObjC, obj);
 }
